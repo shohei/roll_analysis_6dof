56f69bbf2966bfcf011ac665977cba34f915126b
Shohei Aoki <shoaok@gmail.com>
2022-09-07 13:02:33 +0300

    simulink simulation work in progress

--- roll.m
+++ roll.m
@@ -12,13 +12,13 @@ M = [m     0    0 0 0;
      0     0    0 0 1];
 
 %注意：DATCOMではFeetがデフォルト. DIMコマンドで単位を変更できる.
-b_feet = 5.355; %カナード(FINSET1)の最大スパン長[feet].
-b = 0.3048 * b_feet;%m
+% b_feet = 5.355; %カナード(FINSET1)の最大スパン長[feet].
+b = 0.056; %m
 U0 = 100;%飛行速度 100m/s
 rho = 1.293;%kg/m3
 %DATCOMのREF AREAからとった
-S_feet = 11.046;%[feet2]翼面積?
-S = 0.092903 * S_feet;%m2
+% S_feet = 11.046;%[feet2]翼面積?
+S = 0.003; %m2
 
 %FROM DATCOM: 迎角ゼロの場合
 %PAGE23: STATIC AERODYNAMICS FOR BODY-FIN SET 1 AND 2
@@ -63,15 +63,19 @@ C_y_aileron = 0;%記述なし？？
 C_l_aileron = 0 ;%航空機力学入門 p.96, p.97
 C_n_aileron = 0;%航空機力学入門 p.97, p.98 -> 風洞実験?
 
-Sf = ;
-S = ;
-C_L_rudder_fin = ;
-z_f_delta = ;
-b = ;
-V_fin_star = ;
+% Sf = ;
+% S = ;
+Sf_by_S = (3/5.6)^2;
+%C_L_rudder_finが何を指すかわからない.
+% とりあえず「C_yr = 1.554;%CYR in DATCOMを代入してみる
+C_L_rudder_fin = C_yr;
+z_f_delta = 0.03;%ロケット半径 [m]
+%b = ;
+lf = (74-51.3)*0.01;%重心と垂直尾翼空力中心の距離 [m]
+V_fin_star = lf/b*Sf_by_S;
 
-C_y_rudder = Sf/S*C_L_rudder_fin;%航空機力学入門 p.91
-C_l_rudder = z_f_delta/b*Sf/S;%航空機力学入門 p.91
+C_y_rudder = Sf_by_S*C_L_rudder_fin;%航空機力学入門 p.91
+C_l_rudder = z_f_delta/b*Sf_by_S;%航空機力学入門 p.91
 C_n_rudder = -V_fin_star*C_L_rudder_fin;%航空機力学入門 p.101, p.102
 
 Y_xi = rho*U0^2*S/(2*m)*C_y_aileron; 
